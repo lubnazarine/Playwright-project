8dd9722149e0c218b020fb9c18a741b0d524a771
lubnazarine <lubnazarine@gmail.com>
2024-02-10 18:56:18 +0100

    adding first set of basic playwright files in typescript

---
+++ tests/firsttest.spec.ts
@@ -0,0 +1,16 @@
+//include playwright module
+import { test, expect } from '@playwright/test';
+
+//write test
+test('Validate Youtube', async ({ page }) => {
+
+  //Goto URL
+  await page.goto('https://youtube.com/')
+  //Search with keywords
+  await page.getByPlaceholder('Search').click();
+  await page.getByPlaceholder('Search').fill('miss rachel');
+
+  await expect(page.getByRole('button', { name: 'Search' })).toBeEnabled();
+  await page.getByRole('button', { name: 'Search' }).click();
+
+});
